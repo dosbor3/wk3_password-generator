eb8fc52cb7e69382ef8b2fdd96520856a648ba8f
I am Billie <dosbor3@gmail.com>
2021-12-12 23:32:32 -0600

    Final Edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var generatedPassword = "";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -29,42 +31,29 @@ var characterLength = function() {
   return no_chars;
 }
 
-//function to prompt user to select if they wish to include lowercase letters
 var  charSelection = function() {
+  //prompting the user
   //prompting the user
   var choice = window.prompt("Please select password criteria:" +
   "\nLowercase Characters: ENTER 1" +
   "\nUppercase Characters: ENTER 2" +
   "\nSpecial Characters: ENTER 3");
-
-  choice = parseInt(choice);
-
-  //check for valid response
-  if (choice === 1){
-    //randomly generate a lowercase letter (range: 97 - 122)
-    var lowercaseChar = randomNumber(97, 122); 
-    lowercaseChar = String.fromCharCode(lowercaseChar);    
-    //window.alert(lowercaseChar);
-    return lowercaseChar;
-
+  choice = parseInt(choice);    
+  
+  if (choice === 1){ //<-- Return lowercase character
+    var min = 97;
+    var max = 122;
+    return generateCharacter(min, max);
   }
-  else if ( choice == 2){
-    //randomly generate a uppercase letter (range: 65 - 90)
-    var uppercaseChar = randomNumber(65, 90); 
-    uppercaseChar = String.fromCharCode(uppercaseChar);
-    
-    window.alert(uppercaseChar);
-    
-    return uppercaseChar;
+  else if ( choice === 2){ //<-- Return uppercase character
+    var min = 65;
+    var max = 90;
+    return generateCharacter(min, max);
   }
   else if (choice === 3){
-    //randomly generate a special character (range: 33 - 47)
-    var specialChar = randomNumber(33, 47); 
-    specialChar = String.fromCharCode(specialChar);
-    
-    window.alert(specialChar);
-    
-    return specialChar;    
+    var min = 33;
+    var max = 47;
+    return generateCharacter(min, max);    
   }
   else { //Invalid response, continue to prompt user until a valid response is entereed
     window.alert("Invalid Input!\nENTER 1 for YES OR\nENTER 2 for NO");
@@ -72,6 +61,12 @@ var  charSelection = function() {
   }
 }
 
+//function to generate character
+var generateCharacter = function(min, max) {
+  var character = randomNumber(min, max); 
+  character = String.fromCharCode(character);
+  return character;
+}
 
 //function that calls charSelection() based on user character request, and returns password
 var generatePassword = function() {
